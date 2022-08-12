ff6d0cb8b5702ed59783550659b0b5b77384e8e0
GarcGustavo <gustavo-reyes@live.com>
2022-08-12 00:43:55 -0400

    content bootstrapping

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Gustavo Reyes';
+    : 'GarcDev';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Projects and Devlogs';
+    : 'Devlogs';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
