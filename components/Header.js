b4a901bab3517874c9444a141630119d0b3692c2
Gustavo Reyes <gustavo-reyes@live.com>
2023-02-16 15:24:49 -0500

    flex fix

--- components/Header.js
+++ components/Header.js
@@ -8,25 +8,25 @@ export default function Header({ name }) {
     <header className="pt-36 pb-6 pr-12">
           <Container className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hovered-sibling:border-t-0">
             <Row className="flex items-center w-auto pr-10 dark:bg-transparent bg-opacity-0 rounded-3xl justify-center align-center">
-              <Col className="mx-5 mb-5 mt-6">
+              <Col className="flex items-center w-auto mb-5 ml-5">
                 <Image src="/profile.jpeg" className="rounded-full mx-auto opacity-100" alt="" layout='intrinsic' width='150' height='150'></Image>
               </Col>
               <Col>
-                <div className="text-1xl dark:text-white text-left ml-5 mb-2" >
+                <div className="text-1xl dark:text-white text-left ml-5 mb-2 mt-5" >
                     <h1>Gustavo Reyes</h1>
                     <p>Software Engineer</p>
                     <p>greyescarrion@gmail.com</p>
                 </div>
               <Row>
-                <Col className='flex items-center w-auto ml-3'>
+                <Col className='flex items-center w-auto ml-2 mb-5'>
                   <Link href="https://github.com/GarcGustavo">
-                    <a  className="">
+                    <a className="">
                     <img src="https://img.icons8.com/glyph-neue/64/null/github.png"></img>
                     </a>
                   </Link>
                   
                   <Link href="https://www.linkedin.com/in/gustavo-reyes-886423129/">
-                    <a  className="">
+                    <a className="">
                       <img src="https://img.icons8.com/ios-filled/58/null/linkedin-circled--v1.png"/>
                     </a>
                   </Link>
--- pages/index.js
+++ pages/index.js
@@ -13,55 +13,54 @@ export default function Index({ posts, globalData }) {
   return (
     <Layout>
       <SEO title={globalData.name} description={globalData.blogTitle} />
-      <div className="grid grid-flow-col auto-cols-max gap-6 mx-6">
-        <div className="text-center items-center">
-        <Header name={globalData.name}/>
-        <div className="mr-12 pt-4 mt-4 md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b">
-                <div className="prose dark:prose-dark pr-10 dark:bg-transparent bg-opacity-0 rounded-3xl text-1xl dark:text-white ml-5 mb-5" >
-                    <p>
-                      Hello, I am a software engineer based in Rochester, NY. 
-                      I work full-time, but in my free time I like to work on a variety of gamedev projects.
-                      This small blog is a platform for me to share devlogs for some of my personal side-projects and prototypes.
-                      Currently, most of the articles are very brief overviews of tools or systems I previously made. I plan on expanding most of these in the future to have more in-depth explanations.
-                      I also provide links to the relevant repositories at the end of each article, so feel free to check them out!
-                    </p>
-                </div>
+        <div className="flex grid-flow-col auto-cols-max gap-6 mx-6">
+          <div className="text-center items-center">
+          <Header name={globalData.name}/>
+          <div className="mr-12 pt-4 mt-4 md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b">
+                  <div className="prose dark:prose-dark pr-10 dark:bg-transparent bg-opacity-0 rounded-3xl text-1xl dark:text-white ml-5 mb-5" >
+                      <p>
+                        Hello, I am a software engineer based in Rochester, NY. 
+                        I work full-time, but in my free time I like to work on a variety of gamedev projects.
+                        This small blog is a platform for me to share devlogs for some of my personal side-projects and prototypes.
+                        Currently, most of the articles are very brief overviews of tools or systems I previously made. I plan on expanding most of these in the future to have more in-depth explanations.
+                        I also provide links to the relevant repositories at the end of each article, so feel free to check them out!
+                      </p>
+                  </div>
+            </div>
           </div>
-        </div>
-          <main className="w-full mt-12">
-            <h1 className="text-3xl lg:text-5xl text-center mb-12">
-              {globalData.blogTitle}
-            </h1>
-            <ul className="w-full">
-              {posts.map((post) => (
-                <li
-                  key={post.filePath}
-                  className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-                >
-                  <Link
-                    as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
-                    href={`/posts/[slug]`}
+            <main className="w-full mt-12">
+              <h1 className="text-3xl lg:text-5xl text-center mb-12">
+                {globalData.blogTitle}
+              </h1>
+              <ul className="w-full">
+                {posts.map((post) => (
+                  <li
+                    key={post.filePath}
+                    className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
                   >
-                    <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                      {post.data.date && (
-                        <p className="uppercase mb-3 font-bold opacity-60">
-                          {post.data.date}
-                        </p>
-                      )}
-                      <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
-                      {post.data.description && (
-                        <p className="mt-3 text-lg opacity-60">
-                          {post.data.description}
-                        </p>
-                      )}
-                      <ArrowIcon className="mt-4" />
-                    </a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </main>
-
+                    <Link
+                      as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
+                      href={`/posts/[slug]`}
+                    >
+                      <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+                        {post.data.date && (
+                          <p className="uppercase mb-3 font-bold opacity-60">
+                            {post.data.date}
+                          </p>
+                        )}
+                        <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
+                        {post.data.description && (
+                          <p className="mt-3 text-lg opacity-60">
+                            {post.data.description}
+                          </p>
+                        )}
+                        <ArrowIcon className="mt-4" />
+                      </a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </main>
       </div>
       <Footer copyrightText={globalData.footerText} />
       <GradientBackground
